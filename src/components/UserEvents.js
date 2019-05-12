89797f7284e4cebd3097820603a8088579a56a19
Caroline Pisano <pisanocaroline@gmail.com>
2019-05-12 01:57:51 -0400

    filtered forkevents

--- src/components/UserEvents.js
+++ src/components/UserEvents.js
@@ -24,16 +24,35 @@ export default class UserEvents extends Component {
       </button>
       <ul>
         {this.props.userData
-        .map(data => {
+        .filter(events => {
+          return (events.payload.action === 'closed' || events.payload.action === 'opened')
+        })
+        .map((data, i) => {
           return <li key={data.id}>
             <h4>
               <svg className="octicon octicon-repo mr-2 text-gray" viewBox="0 0 12 16" version="1.1" width="12" height="16" aria-hidden="true"><path fillRule="evenodd" d="M4 9H3V8h1v1zm0-3H3v1h1V6zm0-2H3v1h1V4zm0-2H3v1h1V2zm8-1v12c0 .55-.45 1-1 1H6v2l-1.5-1.5L3 16v-2H1c-.55 0-1-.45-1-1V1c0-.55.45-1 1-1h10c.55 0 1 .45 1 1zm-1 10H1v2h2v-1h3v1h5v-2zm0-10H2v9h9V1z"></path></svg>
-              <span className="eventType">{data.type}</span>
+              <span className="eventType">{data.type === 'ForkEvent'? 'Forked Repo' : 'Pull Request'}</span>
               <span className={this.handleStatus(data.payload.action)}>{data.payload.action}</span>
             </h4>
             <a href={`https://github.com/${data.repo.name}`} className="repo-link">{data.repo.name}</a>
+            {/* <p>{data.payload.commits.map(nestedMessage => nestedMessage.message)}</p> */}
           </li>
-        })}
+        })} 
+        {this.props.userData
+        .filter(events => {
+          return (events.payload.action === 'closed' || events.payload.action === 'opened')
+        })
+        .map((data, i) => {
+          return <li key={data.id}>
+            <h4>
+              <svg className="octicon octicon-repo mr-2 text-gray" viewBox="0 0 12 16" version="1.1" width="12" height="16" aria-hidden="true"><path fillRule="evenodd" d="M4 9H3V8h1v1zm0-3H3v1h1V6zm0-2H3v1h1V4zm0-2H3v1h1V2zm8-1v12c0 .55-.45 1-1 1H6v2l-1.5-1.5L3 16v-2H1c-.55 0-1-.45-1-1V1c0-.55.45-1 1-1h10c.55 0 1 .45 1 1zm-1 10H1v2h2v-1h3v1h5v-2zm0-10H2v9h9V1z"></path></svg>
+              <span className="eventType">{data.type === 'ForkEvent'? 'Forked Repo' : 'Pull Request'}</span>
+              <span className={this.handleStatus(data.payload.action)}>{data.payload.action}</span>
+            </h4>
+            <a href={`https://github.com/${data.repo.name}`} className="repo-link">{data.repo.name}</a>
+            {/* <p>{data.payload.commits.map(nestedMessage => nestedMessage.message)}</p> */}
+          </li>
+        })} 
       </ul>
       </>
     )
