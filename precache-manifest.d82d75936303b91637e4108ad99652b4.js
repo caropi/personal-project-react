bd249a6537feb84dfbcc0fb4daf32299cca478b4
Caroline Pisano <pisanocaroline@gmail.com>
2019-05-12 22:21:34 -0400

    Updates

---
+++ precache-manifest.d82d75936303b91637e4108ad99652b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47a96cd99f2c838411b791fa7d890920",
+    "url": "/personal-project-react/index.html"
+  },
+  {
+    "revision": "17e641e2550c82e80157",
+    "url": "/personal-project-react/static/css/main.a6dfbadf.chunk.css"
+  },
+  {
+    "revision": "7ee363d5bb067dc9b033",
+    "url": "/personal-project-react/static/js/2.3a6f0167.chunk.js"
+  },
+  {
+    "revision": "17e641e2550c82e80157",
+    "url": "/personal-project-react/static/js/main.49790b14.chunk.js"
+  },
+  {
+    "revision": "16ccaa2b7d06488c3f8c",
+    "url": "/personal-project-react/static/js/runtime~main.41e8ac32.js"
+  }
+]);
